f28638826d405108f7e7fe6de2d95e43464e2b7b
[Vlad Doniga] <[vladdoniga11@gmail.com]>
2023-12-01 21:20:41 +0100

    day 7

--- AoC_7/src/Main.java
+++ AoC_7/src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         //READ FILE
         StringBuilder input = new StringBuilder();
         try {
-            File inputFile = new File("res/input.txt");
+            File inputFile = new File("res/input2.txt");
             Scanner myReader = new Scanner(inputFile);
             while (myReader.hasNextLine()) {
                 String data = myReader.nextLine();
@@ -20,13 +20,67 @@ public class Main {
             e.printStackTrace();
         }
         String [] instructions = input.toString().split("\n");
-        String [][] instructionsArray = new String[instructions.length][];
+        ArrayList<String[]> instructionsArray = new ArrayList<>();
         for (int i = 0; i < instructions.length; i++) {
-            instructionsArray[i] = instructions[i].split(" ");
+            instructionsArray.add(instructions[i].split(" "));
         }
         //READ FILE
-
-        //Map <String, smth> wires = new HashMap<>();
-        //CIRUIT CONSTRUCTION
+        Map<String, Short> allData = new HashMap<>();
+        while (allData.get("a") == null) {
+        //for (int i = 0; i < 10; i++) {
+            for (String[] instruction : instructionsArray) {
+                switch (instruction.length) {
+                    case 3:
+                        try {
+                            allData.put(instruction[2], Short.parseShort(instruction[0]));
+                        } catch (NumberFormatException e) {
+                            allData.put(instruction[2], allData.get(instruction[0]));
+                        }
+                        break;
+                    case 4:
+                        if (allData.get(instruction[1]) != null) {
+                            allData.put(instruction[3], (short) ~allData.get(instruction[1]));
+                        }
+                        break;
+                    case 5:
+                        switch (instruction[1]) {
+                            case "OR":
+                                try {
+                                    if (allData.get(instruction[2]) != null) {
+                                        allData.put(instruction[4], (short) (Short.parseShort(instruction[0]) | allData.get(instruction[2])));
+                                    }
+                                } catch (NumberFormatException e) {
+                                    if (allData.get(instruction[0]) != null && allData.get(instruction[2]) != null) {
+                                        allData.put(instruction[4], (short) (allData.get(instruction[0]) | allData.get(instruction[2])));
+                                    }
+                                }
+                                break;
+                            case  "AND":
+                                try {
+                                    if (allData.get(instruction[2]) != null) {
+                                        allData.put(instruction[4], (short) (Short.parseShort(instruction[0]) & allData.get(instruction[2])));
+                                    }
+                                } catch (NumberFormatException e){
+                                    if (allData.get(instruction[0]) != null && allData.get(instruction[2]) != null) {
+                                        allData.put(instruction[4], (short) (allData.get(instruction[0]) & allData.get(instruction[2])));
+                                    }
+                                }
+                                break;
+                            case "RSHIFT":
+                                if (allData.get(instruction[0]) != null) {
+                                    allData.put(instruction[4], (short) (allData.get(instruction[0]) >> Integer.parseInt(instruction[2])));
+                                }
+                                break;
+                            case "LSHIFT":
+                                if (allData.get(instruction[0]) != null) {
+                                    allData.put(instruction[4], (short) (allData.get(instruction[0]) << Integer.parseInt(instruction[2])));
+                                }
+                                break;
+                        }
+                        break;
+                }
+            }
+        }
+        System.out.println(allData.get("a"));
     }
 }
